b58a25aa947cbafe0c31deab6b9cc742d0dec8b6
Fahmida Afrin Nadia <fahmida.200547@gmail.com>
2026-01-21 00:57:17 +0600

    Update LinearSearch.cpp

---
+++ LinearSearch.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cout << "Enter value to search: ";
+    cin >> key;
+
+    int pos = -1;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+        {
+            pos = i;
+            break;
+        }
+    }
+
+    if(pos != -1)
+        cout << "Element found at index: " << pos << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}

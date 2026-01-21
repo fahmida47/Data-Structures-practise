20e4797327915485abd59237f4320aeb787b8ca9
Fahmida Afrin Nadia <fahmida.200547@gmail.com>
2026-01-21 10:17:38 +0600

    Implement cycle detection in DFS
    Added cycle detection functionality to DFS algorithm.

--- DFS.cpp
+++ DFS.cpp
@@ -34,5 +34,63 @@ int main()
   return 0;
 }
 
+//---------------------------------------Cycle detect----------------------------//
+#include<bits/stdc++.h>
+using namespace std;
+
+bool cycle = false;
+
+void DFS(int u, vector<vector<int>>& adj, vector<int>& color)
+{
+    color[u] = 1;  // gray (processing)
+    cout << "Visit: " << u << endl;
+
+    for (int v : adj[u]) {
+        if (color[v] == 0) {
+            DFS(v, adj, color);
+        }
+        else if (color[v] == 1) {
+            // back edge found
+            cycle = true;
+        }
+    }
+
+    color[u] = 2;  // black (finished)
+    cout << "Finish: " << u << endl;
+}
+
+int main()
+{
+    int node, edge;
+    cout << "Enter the node:\n";
+    cin >> node;
+
+    cout << "Enter the edges:\n";
+    cin >> edge;
+
+    vector<vector<int>> adj(node);
+
+    cout << "Enter directed edges (u v):\n";
+    for (int i = 0; i < edge; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    vector<int> color(node, 0); // 0 = white
+
+    for (int i = 0; i < node; i++) {
+        if (color[i] == 0)
+            DFS(i, adj, color);
+    }
+
+    if (cycle)
+        cout << "\nCycle detected → Impossible to complete the course\n";
+    else
+        cout << "\nNo cycle → Possible to complete the course\n";
+
+    return 0;
+}
+
 
 
